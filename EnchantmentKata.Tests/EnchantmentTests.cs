94e826080c980f393b479d1b4684c6f38663d3ed
Daniel Barnett <b4barnett@googlemail.com>
2022-02-03 19:58:28 +0000

    All single set enchantments are working

--- EnchantmentKata.Tests/EnchantmentTests.cs
+++ EnchantmentKata.Tests/EnchantmentTests.cs
@@ -30,7 +30,11 @@ namespace EnchantmentKata.Tests
 
             _enchantments = new Dictionary<string, Enchantment>()
             { 
-                { "Fire", new Enchantment( "Fire", "Inferno ", "+5 fire damage" ) } 
+                { "Fire", new Enchantment( "Fire", "Inferno ", "+5 fire damage" ) },
+                { "Ice", new Enchantment( "Ice", "Icy ", "+5 ice damage" ) },
+                { "LifeSteal", new Enchantment( "LifeSteal", "Vampire ", "+5 life steal" ) },
+                { "Agility", new Enchantment( "Agility", "Quick ", "+5 agility") },
+                { "Strength", new Enchantment( "Strength", "Angry ", "+5 strength" ) }
             };
         }
 
@@ -45,10 +49,10 @@ namespace EnchantmentKata.Tests
 
         //EnchantmentKey, Expected Name, Expected Effect
         [TestCase( "Fire", "Inferno ", "+5 fire damage")]
-        [TestCase( "Ice", "Icy", "+5 ice damage")]
-        [TestCase( "LifeSteal", "Vampire", "+5 life steal")]
-        [TestCase( "Agility", "Quick", "+5 agility")]
-        [TestCase( "Strength", "Angry", "+5 strength") ]
+        [TestCase( "Ice", "Icy ", "+5 ice damage")]
+        [TestCase( "LifeSteal", "Vampire ", "+5 life steal")]
+        [TestCase( "Agility", "Quick ", "+5 agility")]
+        [TestCase( "Strength", "Angry ", "+5 strength") ]
         public void EnchantmentTest( string enchantment, string name, string effect)
         {
             _mockEnchantmentProvider.Setup( x => x.GetRandomEnchantment() )
